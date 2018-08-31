7f65077147c3195c6e522b8289e918e6105e6165
AlexAlvisi <alex.alvisi.80@gmail.com>
2018-08-31 16:29:14 +0200

    Initial commit

---
+++ Website/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+  <h1>Home</h1>
+  <p>Lorem ipsum dolor</p>
+@endsection
+
+@section('sidebar')
+  @parent
+  <p>This is appended to the sidebar</p>
+@endsection
